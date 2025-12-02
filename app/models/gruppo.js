6dcba75c74eabdc2131cd631d7e56d8bf5487eb2
Axlander36 <axlander36@gmail.com>
2025-12-02 17:01:30 +0100

    Aggiunta relazione gruppi-utenti

--- app/models/gruppo.js
+++ app/models/gruppo.js
@@ -5,5 +5,9 @@ export default mongoose.model('Gruppo', new Schema({
     nome: String,
     idPercorso: String,
     esperienza: String,
-    data: Date
+    data: Date,
+    utenti: [{
+        type: Schema.Types.ObjectId,
+        ref: "utente"
+    }]
 }));
